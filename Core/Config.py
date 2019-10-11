06071f8cfeafe55ddbe0b9dc845130296d710782
LavaPower <lavapower84@gmail.com>
2019-10-11 12:00:06 +0200

    Add config

---
+++ Core/Config.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+
+class Config:
+    def __init__(self):
+        with open(os.path.join(os.path.dirname(__file__), "..", "config.json"), "r") as f:
+            self.config = json.load(f)
+
+    def get(self, key, default):
+        keys = key.split(".")
+        if len(keys) == 1:
+            return self.config.get(key, default)
+        else:
+            value = self.config
+            for i in keys:
+                if i != keys[-1]:
+                    value = value[i]
+                else:
+                    value = value.get(i, default)
+            return value
--- Core/Factory.py
+++ Core/Factory.py
@@ -20,5 +20,10 @@ class Factory(ServerFactory):
         self.logger.setLevel(self.log_level)
         self.logger.info("Server started.")
 
+        self.config = Config()
+
+        self.motd = self.config.get("server.motd", "PyMine Server")
+        self.online_mode = self.config.get("server.online_mode", True)
+        self.max_players = self.config.get("server.max_players", 10)
         self.registry = LookupRegistry.from_json(os.path.join(os.path.dirname(__file__), "Datas"))
 
