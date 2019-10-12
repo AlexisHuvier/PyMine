48c82558a6292b72280f3010f1fb700aff9b447d
LavaPower <lavapower84@gmail.com>
2019-10-13 01:22:23 +0200

    Create class for Command

--- Core/CommandManager.py
+++ Core/CommandManager.py
@@ -1,3 +1,17 @@
+class Command:
+    def __init__(self, name, function,  description="", permission=0):
+        self.name = name
+        self.function = function
+        self.permission = permission
+        self.description = description
+
+    def execute(self, ctx, *args):
+        if self.permission == 1 and not ctx.is_op(ctx.protocol.display_name):
+            ctx.chat.send_to(ctx.protocol, "Vous n'êtes pas op.")
+        else:
+            self.function(ctx, *args)
+
+
 class CommandContext:
     def __init__(self, protocol):
         self.chat = protocol.core_plugins["chat"]
@@ -8,20 +22,22 @@ class CommandContext:
 class CommandManager:
     def __init__(self, factory):
         self.factory = factory
-        self.commands = {}
+        self.commands = []
         self.ops = factory.config.get("ops", [])
 
-    def register(self, command, function):
-        if command in self.commands.keys():
-            self.factory.logger.warn("Command already exist : " + command)
-        else:
-            self.commands[command] = function
+    def register(self, command, function, description="", permission=0):
+        for i in self.commands:
+            if i.name == command:
+                self.factory.logger.warn("Command already exist : "+command)
+                return
+        self.commands.append(Command(command, function, description, permission))
 
     def call_command(self, protocol, message):
         command, *args = message.split(" ")
-        if command in self.commands.keys():
-            self.commands[command](CommandContext(protocol), *args)
-        else:
-            protocol.core_plugins["chat"].send_to(
-                protocol,
-                self.factory.config.get("messages.command_not_exist", "La commande {} n'existe pas.").format(command))
+        for i in self.commands:
+            if i.name == command:
+                i.execute(CommandContext(protocol), *args)
+                return
+        protocol.core_plugins["chat"].send_to(protocol,
+                                              self.factory.config.get("messages.command_not_exist",
+                                                                      "La commande {} n'existe pas.").format(command))
