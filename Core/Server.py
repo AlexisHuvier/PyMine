083868b35b6b4dbc497b7d4f6966d1244fa74d83
LavaPower <lavapower84@gmail.com>
2019-10-23 16:17:14 +0200

    Rework on logging (Set your log_level in config)

--- Core/Server.py
+++ Core/Server.py
@@ -10,12 +10,27 @@ from Core.Config import Config
 from Core.PluginManager import PluginManager
 from Core.CommandManager import CommandManager
 
+levels = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL
+}
+
 
 class Server(ServerFactory):
     protocol = Player
 
     def __init__(self):
         super(Server, self).__init__()
+
+        self.config = Config()
+
+        self.log_level = levels[self.config.get("server.log_level", "info")]
+        self.motd = self.config.get("server.motd", "PyMine Server")
+        self.online_mode = self.config.get("server.online_mode", True)
+        self.max_players = self.config.get("server.max_players", 10)
 
         self.logger = logging.getLogger("Server")
         self.logger.setLevel(self.log_level)
@@ -27,14 +42,8 @@ class Server(ServerFactory):
         self.logger.addHandler(filehandler)
         self.logger.info("Server started.")
 
-        self.config = Config()
-
-        self.motd = self.config.get("server.motd", "PyMine Server")
-        self.online_mode = self.config.get("server.online_mode", True)
-        self.max_players = self.config.get("server.max_players", 10)
         self.command_manager = CommandManager(self)
         self.plugin_manager = PluginManager(self, self.config.get("plugins", []))
         self.plugin_manager.call("start", self)
 
         self.registry = LookupRegistry.from_json(os.path.join(os.path.dirname(__file__), "Datas"))
-
--- Plugins/Demo/demo.py
+++ Plugins/Demo/demo.py
@@ -3,50 +3,51 @@ class Demo:
         self.logger = None
 
     def start(self, server):
-        server.command_manager.register("demo", self.command_info, "Info du plugin Demo")
+        server.command_manager.register(self, "demo", self.command_info, "Info du plugin Demo")
         self.logger = server.logger
-        self.logger.info("Demo : START. (Server : %s )", server)
+        self.logger.debug("Demo : START. (Server : %s )", server)
 
     def player_joined(self, player):
-        self.logger.info("Demo : PLAYER_JOINED. (Player : %s )", player)
+        self.logger.debug("Demo : PLAYER_JOINED. (Player : %s )", player)
 
     def player_left(self, player):
-        self.logger.info("Demo : PLAYER_LEFT. (Player : %s )", player)
+        self.logger.debug("Demo : PLAYER_LEFT. (Player : %s )", player)
+
     def player_move(self, player, x, y, z):
         self.logger.debug("Demo : PLAYER_MOVE. (Player : %s. Pos : %s )", player, [x, y, z])
 
     def chat_message(self, message):
-        self.logger.info("Demo : CHAT_MESSAGE. (Message : %s )", message)
+        self.logger.debug("Demo : CHAT_MESSAGE. (Message : %s )", message)
 
     def block_placement(self, player, block_info):
-        self.logger.info("Demo : BLOCK_PLACEMENT. (Player : %s. Info : %s )", player, block_info)
+        self.logger.debug("Demo : BLOCK_PLACEMENT. (Player : %s. Info : %s )", player, block_info)
 
     def use_item(self, player, hand):
-        self.logger.info("Demo : USE_ITEM. (Player : %s. Hand : %s )", player, hand)
+        self.logger.debug("Demo : USE_ITEM. (Player : %s. Hand : %s )", player, hand)
 
     def start_digging(self, player, x, y, z, face):
-        self.logger.info("Demo : START_DIGGING. (Player : %s. Pos : %s. Face : %s )", player, [x, y, z], face)
+        self.logger.debug("Demo : START_DIGGING. (Player : %s. Pos : %s. Face : %s )", player, [x, y, z], face)
 
     def cancel_digging(self, player, x, y, z, face):
-        self.logger.info("Demo : CANCEL_DIGGING. (Player : %s. Pos : %s. Face : %s )", player, [x, y, z], face)
+        self.logger.debug("Demo : CANCEL_DIGGING. (Player : %s. Pos : %s. Face : %s )", player, [x, y, z], face)
 
     def finish_digging(self, player, x, y, z, face):
-        self.logger.info("Demo : FINISH_DIGGING. (Player : %s. Pos : %s. Face : %s )", player, [x, y, z], face)
+        self.logger.debug("Demo : FINISH_DIGGING. (Player : %s. Pos : %s. Face : %s )", player, [x, y, z], face)
 
     def drop_item(self, player):
-        self.logger.info("Demo : DROP_ITEM. (Player : %s )", player)
+        self.logger.debug("Demo : DROP_ITEM. (Player : %s )", player)
 
     def shoot_arrow(self, player):
-        self.logger.info("Demo : SHOOT_ARROW. (Player : %s )", player)
+        self.logger.debug("Demo : SHOOT_ARROW. (Player : %s )", player)
 
     def finish_eating(self, player):
-        self.logger.info("Demo : FINISH_EATING. (Player : %s )", player)
+        self.logger.debug("Demo : FINISH_EATING. (Player : %s )", player)
 
     def swap_hand(self, player):
-        self.logger.info("Demo : SWAP_HAND. (Player : %s )", player)
+        self.logger.debug("Demo : SWAP_HAND. (Player : %s )", player)
 
     def command_info(self, ctx, *args):
-        self.logger.info("Demo : COMMAND_INFO.")
+        self.logger.debug("Demo : COMMAND_INFO.")
         ctx.chat.send_to(ctx.player, "[PLUGIN] Demo Plugin created by LavaPower")
 
 
