bbc24a36399e86bea740f05b67305f575457eb93
LavaPower <lavapower84@gmail.com>
2019-10-23 16:14:07 +0200

    Load/Unload plugin

--- Core/PluginManager.py
+++ Core/PluginManager.py
@@ -28,3 +28,20 @@ class PluginManager:
                 fnc = getattr(i.instance, function)
                 fnc(*args)
 
+    def load(self, plugin):
+        for v in self.plugins:
+            if v.directory == plugin:
+                self.factory.logger.warning("Plugin already loaded : {}".format(plugin))
+                return
+        self.plugins.append(Plugin(plugin))
+        self.plugins[-1].instance.start(self.factory)
+        self.factory.logger.info("Load plugin : {}".format(plugin))
+
+    def unload(self, plugin):
+        for k, v in enumerate(self.plugins):
+            if v.directory == plugin:
+                del self.plugins[k]
+                self.factory.logger.info("Unload plugin : {}".format(plugin))
+                return
+        self.factory.logger.warning("Plugin already unload : {}".format(plugin))
+
